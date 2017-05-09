aae8f2284ef52bf4afd6240819eff8c4c213c1f1
Kamilah Uddin <kamilah.uddin@gmail.com>
2017-05-08 17:14:47 -0700

    loops years for data and posts to table

--- src/controller.ts
+++ src/controller.ts
@@ -3,20 +3,23 @@ namespace angularLiteServer {
   export class appController{
     public message: string = 'Your data will show up here';
     public position: string;
-    public year = 2011;
+    public years: number[] = [];
     public Players: Player[] = [];
-    public roster;
+    public playerName;
+
+    // public roster;
 
     constructor(
         private $http:ng.IHttpService
 
-        ) {
-
+        ) {for (var i=0; i<52;i++){
+            this.years[i] = 1976 + i
+          }
+          console.log (this.years)
           }
 
-          public loadRoster(year)
+          public loadRoster(year){
 
-          {
             this.$http.get(`/seeds/roster-${year}.json`).then((res)=>{
               this.Players = <Player[]>res.data;
               console.log(<Player[]>res.data);
